fddc99a835f564f0a2f53a4b02d476aea59951cf
Trevor Fountain <doches@gmail.com>
2008-10-02 15:19:09 +0100

    Fixed setup() bug in which screen list was left empty.

--- plugins/Random/Random.rb
+++ plugins/Random/Random.rb
@@ -5,11 +5,15 @@ class Random < Plugin
     @tick = Time.now.to_i
     @interval = Plugin.preferences['Random.interval'].to_i
     @interval ||= 5
+  end
+  
+  def load
     @list = Plugin.loader.screens.screenlist
     @current = @list.index("#{Plugin.loader.screens.current.class}".to_sym)
   end
   
   def update
+    load if @list.nil?
     tock = Time.now.to_i
     if tock - @tick > @interval
       @tick = tock
